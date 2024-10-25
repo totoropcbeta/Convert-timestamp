0b8c5102efe7d8883eae581bf8bc08cd7f19e50c
yuanhang08 <yuanhang08@meituan.com>
2024-10-25 17:41:20 +0800

    月日格式

--- src/utils.js
+++ src/utils.js
@@ -28,6 +28,8 @@ function getTimeString(timestamp, type) {
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
     let milliseconds = date.getMilliseconds();
+    month = month < 10 ? "0" + month : month;
+    day = day < 10 ? "0" + day : day;
     hours = hours < 10 ? "0" + hours : hours;
     minutes = minutes < 10 ? "0" + minutes : minutes;
     seconds = seconds < 10 ? "0" + seconds : seconds;
